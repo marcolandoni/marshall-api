0c7a4eaa72bf484c4011039617e9d53794f597ee
marcolandoni <marco.landoni@gmail.com>
2025-08-12 16:46:11 +0200

    login working

--- app.py
+++ app.py
@@ -1,19 +1,100 @@
-import mysql.connector
+import logging
 
-print ( "Hello World! " )
+from fundamentals.mysql import database, readquery
+from packages.login import login_user
 
-mydb = mysql.connector.connect( host="127.0.0.1",
-  user="marshall",
-  password="mar5ha11",
-  database="marshall"
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity, create_access_token, get_jwt
+from datetime import timedelta
+import redis
+
+
+
+logging.basicConfig(filename='myapp.log', level=logging.INFO)
+log = logging.getLogger(__name__)
+
+
+dbSettings = {
+    'host': '127.0.0.1', 
+    'user': 'marshall', 
+    'password': 'mar5ha11', 
+    'db': 'marshall'
+}
+
+dbConn = database(
+    log=log,
+    dbSettings=dbSettings
+).connect()
+
+app = Flask(__name__)
+ACCESS_EXPIRES = timedelta(hours=24)
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=1)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = ACCESS_EXPIRES
+
+
+jwt = JWTManager(app)
+
+jwt_redis_blocklist = redis.StrictRedis(
+    host="localhost", port=6379, db=0, decode_responses=True
 )
 
-print(mydb)
+@jwt.token_in_blocklist_loader
+def check_if_token_is_revoked(jwt_header, jwt_payload: dict):
+    jti = jwt_payload["jti"]
+    token_in_redis = jwt_redis_blocklist.get(jti)
+    return token_in_redis is not None
+
+@app.route("/login", methods=["POST"])
+def login():
+  try:
+    firstname = request.json.get("firstname", None)
+    lastname = request.json.get("lastname", None)
+    password = request.json.get("password", None)
+
+    return login_user(dbConn, firstname, lastname, password, log)
+  except:
+    return jsonify({"msg": "Internal Server Error"}), 505
 
 
-cursor = mydb.cursor(buffered=True, dictionary=True)
-cursor.execute("SELECT * from webapp_users;")
-results = cursor.fetchall()
-print(results)
-for r in results:
-    print(r)
+@app.route("/refresh", methods=["POST"])
+@jwt_required(refresh=True)
+def refresh():
+    identity = get_jwt_identity()
+    access_token = create_access_token(identity=identity, fresh=False)
+    return jsonify(access_token=access_token)
+
+
+@app.route("/logged_user", methods=["GET"])
+@jwt_required()
+def logged_user():
+    # Access the identity of the current user with get_jwt_identity
+    try:
+      current_user = get_jwt_identity()
+      print(current_user)
+      return jsonify(logged_in_as=current_user), 200
+    except Exception as e:
+      return jsonify({"msg": "Internal Server Error"}), 505
+
+
+@app.route("/logout", methods=["DELETE"])
+@jwt_required(verify_type=False)
+def logout():
+  try:
+      token = get_jwt()
+      jti = token["jti"]
+      ttype = token["type"]
+      jwt_redis_blocklist.set(jti, "", ex=ACCESS_EXPIRES)
+
+      # Returns "Access token revoked" or "Refresh token revoked"
+      return jsonify(msg=f"{ttype.capitalize()} token successfully revoked")
+  except Exception as e:
+    print(e)
+
+if __name__ == "__main__":  
+    app.run()
---
+++ packages/login.py
@@ -0,0 +1,34 @@
+from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+from flask import jsonify
+
+from fundamentals.mysql import database, readquery
+from passlib.hash import sha256_crypt
+
+def login_user(dbConn, firstname, secondname, password, log):
+    # GETTING THE DATA FROM THE DATABASE
+    query = f"SELECT * from webapp_users WHERE firstname = '{firstname}' AND secondname='{secondname}'"
+    rs = readquery(
+        query,
+        dbConn,
+        log
+    )
+    if len(rs) < 0:
+        return jsonify({"msg": "Bad username or password"}), 401
+    else:
+        try:
+            # GETTING THE HASHED PASSWORD TO BE COMPARED WITH THE PASSED ONE
+            hash_psw = rs[0]['password']
+            result_check = sha256_crypt.verify(password, hash_psw)
+            if result_check:
+                access_token = create_access_token(identity=f"{firstname}.{secondname}")
+                refresh_token = create_refresh_token(identity=f"{firstname}.{secondname}")
+                return jsonify(access_token=access_token, refresh_token=refresh_token)
+            else:
+                return jsonify({"msg": "Bad username or password"}), 401
+        except:
+            return jsonify({"msg": "Internal Server Error"}), 505
+
+            
